26080190f0f039176fc699da888f77c2237374bb
zumbalogy <intrapostal@gmail.com>
2014-06-15 23:20:59 -0400

    init and gems

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+
+gem 'sass-rails', '~> 4.0.0'
+
+gem 'uglifier', '>= 1.3.0'
+
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'jquery-rails'
+
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+
+gem 'mongoid', github: 'mongoid/mongoid'
+gem 'bson_ext'
+gem "ember-rails"
+gem "devise"
+gem 'rails_12factor', group: :production
+
+
+group :development do
+  gem "pry"
+  gem "pry-debugger"
+  # gem "bullet"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emberwave::Application.config.secret_key_base = '9b9f23f17f1ca99720b8c7e9a112cd8c35c655e42110d6723ec20a77b901c814af9b01ef86c2c139134729549e9c29e0fbf30d1034528801b0e1f8c50b26e3dc'
